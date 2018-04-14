267b41929746ff0e45f196cf6faffc204e81042b
Noah Kovacs <kovacsn66@gmail.com>
2018-04-14 11:43:03 -0500

    obfuscate bot token
    Signed-off-by: Noah Kovacs <kovacsn66@gmail.com>

--- src/config.ts
+++
@@ -1,5 +0,0 @@
-export default class Config {
-  constructor() {}
-  secret: string = '-t_6IYG3pcQQ5-8WaWVsmlHzja6F_WDk';
-  token: string = 'NDI4NzI0OTU4OTU3NjY2MzA0.DawpgQ.WE1Jclip5Ka0K904FAR47Zx-e1Y'
-}
